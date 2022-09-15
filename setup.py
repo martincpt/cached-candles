a80d3692102d8c776760ae2842a2d53fc5a3868c
Martin Trapp <martincpt@gmail.com>
2022-09-15 09:37:55 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+"""Install packages as defined in this file into the Python environment."""
+from setuptools import setup, find_packages
+
+# The version of this tool is based on the following steps:
+# https://packaging.python.org/guides/single-sourcing-package-version/
+VERSION = {}
+REQUIREMENTS = [i.strip() for i in open("requirements.txt").readlines()]
+
+with open("./cached_candles/__init__.py") as fp:
+    # pylint: disable=W0122
+    exec(fp.read(), VERSION)
+
+setup(
+    name="cached_candles",
+    author="Martin Trapp",
+    author_email="info@martintrapp.com",
+    description="Fetches crypto candles with cache.",
+    version=VERSION.get("__version__", "0.0.0"),
+    packages=find_packages(where=".", exclude=["tests"]),
+    install_requires=REQUIREMENTS,
+    classifiers=[
+        "Programming Language :: Python :: 3.0",
+        "Topic :: Utilities",
+        "Development Status :: 3 - Alpha",
+    ],
+)
